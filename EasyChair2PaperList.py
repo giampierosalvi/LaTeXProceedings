02e38ce05797bec990ca59d8a67b6cb8d31fd732
Giampiero Salvi <giampi@kth.se>
2017-06-15 10:16:40 +0200

    clean-up, substitution of and in author list

--- EasyChair2PaperList.py
+++ EasyChair2PaperList.py
@@ -1,15 +1,19 @@
-# EasyChair2PaperList: extracts list of authors, title and submission number for each
-#    submission in a EasyChair Conference and outputs a LaTeX file that can be imported
-#    by the LaTeXProceedings class to create the conference proceedings.
+# EasyChair2PaperList
+# Extracts list of authors, title and submission number for each submission
+# in a EasyChair Conference and outputs a LaTeX file that can be imported
+# by the LaTeXProceedings class to create the conference proceedings.
 #
 # Usage:
-# 1) right click and download the html for the List of Submission page from easychair.org
-#    into (for example) ListOfSubmissions.html
-# 2) Download submissions and note the pdfBaseName (file name up to submission number),
-#    for example, if the file names are in the form papers/GLU2017_paper_10.pdf,
-#    pdfBaseName="papers/GLU2017_paper_"
+# 1) right click and download the html for the List of Submission page from
+#    easychair.org into (for example) ListOfSubmissions.html
+# 2) Download submissions and note the pdfBaseName (file name up to submission
+#    number), for example, if the file names are in the form
+#    papers/GLU2017_paper_10.pdf, then pdfBaseName="papers/GLU2017_paper_"
 # 3) run:
 #    python EasyChair2PaperList.py ListOfSubmissions.html pdfBaseName > papers.tex
+#
+# Note: in case of multiple authors, the final " and" is substituted with a ","
+#   to comply with the LaTeXProceedings class.
 #
 # (C) 2017, Giampiero Salvi <giampi@kth.se>
 from bs4 import BeautifulSoup
@@ -19,20 +23,25 @@ htmlFileName = sys.argv[1]
 pdfBaseName = sys.argv[2]
 
 with open(htmlFileName, 'r') as f:
-    html=f.read()
+    htmlpage=f.read()
 # parse the whole page
-soup = BeautifulSoup(html, "lxml")
+soupObj = BeautifulSoup(htmlpage, "lxml")
 # read submission table
-table = soup.find("table", attrs={"class":"ct_table"})
+table = soupObj.find("table", attrs={"class":"ct_table"})
 # get headings
 headings = [th.get_text() for th in table.find("tr").find_all("th")]
+# Get indexes for relevant columns
+idxs = [headings.index("#"), headings.index("Authors"), headings.index("Title")]
 # get data row by row
-datasets = []
+submissions = []
 for row in table.find_all("tr")[1:]:
     elements = [td.get_text() for td in row.find_all("td")]
-    dataset={}
-    for idx in range(3):
-        dataset[headings[idx]] = elements[idx]
-    datasets.append(dataset)
-for dataset in datasets:
-    print("\includepaper{"+dataset['Title']+"}{"+dataset['Authors']+"}{"+pdfBaseName+dataset['#']+"}")
+    submission={}
+    for idx in idxs:
+        submission[headings[idx]] = elements[idx]
+    submissions.append(submission)
+for submission in submissions:
+    title = submission['Title']
+    authors = submission['Authors'].replace(" and", ",")
+    fileBaseName = pdfBaseName+submission['#']
+    print("\includepaper{"+title+"}{"+authors+"}{"+fileBaseName+"}")
